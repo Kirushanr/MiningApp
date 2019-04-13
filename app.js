81e44ed2a0c996332ea4dba93e46d8e08ae5a1d9
kirushan <kirushan22@gmail.com>
2019-04-13 10:38:21 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// import environmental variables from our variables.env file
+const env = require('dotenv').config();
+
+// Connect to our Database and handle any bad connections
+mongoose.connect(process.env.DB_HOST);
+console.log(process.env.DB_HOST)
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', (err) => {
+  
+});
+
+
+// import all of our models
+require('./models/Assessment');
+
+// Start our app!
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
