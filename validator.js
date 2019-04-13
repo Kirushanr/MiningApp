e15fd552598c77747077465adb4eb71cc3b2072a
kirushan <kirushan22@gmail.com>
2019-04-13 20:32:04 +1000

    Create validator to validate inputs

---
+++ validator.js
@@ -0,0 +1,38 @@
+const { check } = require('express-validator/check');
+const { sanitize } = require('express-validator/filter');
+const { body } = require('express-validator/check');
+
+exports.validate = (method) => {
+    switch (method) {
+        case 'POST': {
+            return [
+                body('assesmentId')
+                .isInt().withMessage('Please enter a valid assesment number E.g. 19465 '),
+                
+                body('vendorName').not()
+                .isEmpty().withMessage('Please enter vendor name').trim().escape(),
+
+                body('safety')
+                .isInt({gt: 0, lt: 6}).withMessage('Safety value must be between 1-5'),
+
+                body('safetyComment').not()
+                .isEmpty().withMessage('Please enter your comment on safety')
+                .isLength({max:150}).withMessage("Safety cannot exceed 150 characters")
+                .trim().escape(),
+
+                body('quality').isIn(['Bad', 'Average', 'Good', 'Excellent'])
+                .withMessage('Enter a quality (Bad, Average, Good or Excellent)')
+                .trim().escape(),
+
+                body('qualityComment').not()
+                .isEmpty().withMessage('Please enter your comment on quality')
+                .isLength({max:150}).withMessage("Safety comment should not exceed 150 characters")
+                .trim().escape(),
+
+                body('Notes').trim().escape()
+
+            ];
+        }
+    }
+};
+
