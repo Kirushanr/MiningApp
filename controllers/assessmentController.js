8010a5a309b0a2fdcfa16c38780e18a86130204d
kirushan <kirushan22@gmail.com>
2019-04-13 20:33:56 +1000

    Add validation for creating assesment (POST)

--- controllers/assessmentController.js
+++ controllers/assessmentController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const errorHandler = require('../errorHandler');
 const Assessment = mongoose.model('Assessment');
+const { validationResult } = require('express-validator/check');
 
 exports.getAssesments = (req, res) => {
     res.send('TO BE IMPLEMENTED');
@@ -8,26 +9,34 @@ exports.getAssesments = (req, res) => {
 
 //get an assesment given an id
 exports.getAssesment = (req, res) => {
-    console.log(req.params.id)
-    const assesment = Assessment.findOne({assesmentId:req.params.id}).exec();
-    assesment.then(document =>{
-        if(!document){
-            res.status(200).json({"message":"Assesment detail not found"});
+    
+    const assesment = Assessment.findOne({ assesmentId: req.params.id }).exec();
+    assesment.then(document => {
+        if (!document) {
+            res.status(200).json({ "message": "Assesment detail not found" });
         }
-        res.status(200).json({message:"Assement found", data: document});
+        res.status(200).json({ message: "Assement found", data: document });
     }).catch(error => {
-        console.log(error);
+        if (error.name === 'CastError') {
+            res.status(422).json({ 'message': 'Please enter a correct assesment id (Eg. 1943)' });
+        }
     });
 };
 
-
+//create a new assesment
 exports.createAssesment = (req, res) => {
-    
+
+    //validate & sanitize the input
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
     const newAssesment = new Assessment(req.body);
     newAssesment.save()
         .then((document) => res.json({ status: 200, message: 'Assessment created successfully', data: document }))
         .catch(error => {
             let message = errorHandler.getErrorMessage(error);
             res.status(500).json({ status: 500, message: message });
-    });
+        });
 };
