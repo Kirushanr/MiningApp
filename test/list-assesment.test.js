67c71c3efe00c778882ba349d5c0358e14a2e941
kirushan <kirushan22@gmail.com>
2019-04-14 15:26:33 +1000

    Update list-assesment.js
    Fixed spelling errors.
    Formatted code,
    added test cases for
    1) non integer passed as assesment id
    2) assesment detail is not found in the database

--- test/list-assesment.test.js
+++ test/list-assesment.test.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Assessment  = require('../models/Assessment');
+const Assessment = require('../models/Assessment');
 
 
 //Require the test-dependencies
@@ -11,52 +11,88 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('Removing records', function() {
-    beforeEach(function(done) {
-        Assessment.remove().exec()
-        .then(()=>{
-            done();
+describe('Test the GET /api/assessment/:id route', function () {
+
+    
+    beforeEach(function (done) {
+            Assessment.deleteMany().exec()
+                .then(() => {
+                    done();
+                });
+    });
+        
+    
+
+    context('assessment id passed as route parameter',() => {
+
+        it('it should retrive the assessment by the given assessment id', (done) => {
+            const assessment = new Assessment({
+                assesmentId: 195564,
+                vendorName: 'Apple Inc',
+                safety: 4,
+                safetyComment: 'Very safe equipment',
+                quality: 'Good',
+                qualityComment: 'Exceeds expectation',
+                Notes: 'None'
+
+            });
+
+            assessment.save((error, document) => {
+
+                chai.request(server)
+                    .get('/api/assessment/' + document.assesmentId)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('data');
+                        res.body.data.should.have.property('safety');
+                        res.body.data.should.have.property('safetyComment');
+                        res.body.data.should.have.property('quality');
+                        res.body.data.should.have.property('qualityComment');
+                        res.body.data.should.have.property('Notes');
+                        res.body.data.should.have.property('assesmentId').eql(document.assesmentId);
+                        done();
+                    });
+            });
+        });
+
+        it('should return Assessment detail not found', (done)=>{
+            chai.request(server)
+                .get('/api/assessment/' +195564)
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('message');
+                    res.body.message.should.equal('Assessment detail not found');
+                    done();
+                });
+        });
+    });
+
+    context('non integer passed as assessment id',()=>{
+        it('should return error ', (done) => {
+            chai.request(server)
+                .get('/api/assessment/' +'12a')
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    done();
+                });
         });
     });
 
 
-    /*
-    * Test the /GET/:id route
-    */
-  describe('/GET/:id book', () => {
-      it('it should retrive an assesment by the given assesment id', (done) => {
-         const assessment = new Assessment({
-            assesmentId:195564,
-            vendorName:'Apple Inc',
-            safety : 4,
-            safetyComment:'Very safe equipment',
-            quality:'Good',
-            qualityComment:'Exceeds expectation',
-            Notes:'None'
-            
-         });
-        
-        assessment.save((error,document) => {
-              
-              chai.request(server)
-                  .get('/api/assesment/' + document.assesmentId)
-                  .send(document)
-                  .end((err, res) => {
-                      res.should.have.status(200);
-                      res.body.should.be.a('object');
-                      res.body.should.have.property('data');
-                      res.body.data.should.have.property('safety');
-                      res.body.data.should.have.property('safetyComment');
-                      res.body.data.should.have.property('quality');
-                      res.body.data.should.have.property('qualityComment');
-                      res.body.data.should.have.property('Notes');
-                      res.body.data.should.have.property('assesmentId').eql(document.assesmentId);
-                      done();
-                  });
-          });
-      });
-  });
+    context('not passing assessment id as route parameter',()=>{
+        it('it should return 404 ', (done) => {
+            chai.request(server)
+                .get('/api/assessment/')
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    done();
+                });
 
-
+        });
+    });
 });
 
