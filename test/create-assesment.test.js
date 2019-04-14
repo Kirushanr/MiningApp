34a76732ac198af3259f79594366d75a206ff058
kirushan <kirushan22@gmail.com>
2019-04-14 18:10:15 +1000

    create testcases
    added testcases to test the following
    1) Test required fields.
    2) safety field boundry values.
    3) quality values in gooo/bad/average/excellent

--- test/create-assesment.test.js
+++ test/create-assesment.test.js
@@ -18,7 +18,7 @@ describe('Test the POST /api/assessment/create route', function () {
         Assessment.deleteMany().exec()
             .then(() => {
                 done();
-            }).catch(error=>{
+            }).catch(error => {
                 console.log(error);
             });
     });
@@ -27,7 +27,7 @@ describe('Test the POST /api/assessment/create route', function () {
         let assessment = {
             assessmentId: 195564,
             vendorName: 'Apple Inc',
-            safety: 4,
+            safety: 1,
             safetyComment: 'Very safe equipment',
             quality: 'Good',
             qualityComment: 'Exceeds expectation',
@@ -35,37 +35,111 @@ describe('Test the POST /api/assessment/create route', function () {
         };
 
         chai.request(server)
-        .post('/api/assessment')
-        .send(assessment)
-        .end((err, res) =>{
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('data');
-            res.body.data.should.have.property('assessmentId').eql(assessment.assessmentId);
-           done();  
-        });
+            .post('/api/assessment')
+            .send(assessment)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('data');
+                res.body.data.should.have.property('assessmentId').eql(assessment.assessmentId);
+                done();
+            });
 
     });
 
-    context('with invalid inputs',()=>{
-            it('should return error when required parameters not passed', (done)=> {
+    context('## with invalid inputs', () => {
+        it('should return errors when required parameters not passed', (done) => {
+            let assessment = {
+                Notes: 'None'
+            };
+
+            chai.request(server)
+                .post('/api/assessment')
+                .send(assessment)
+                .end((err, res) => {
+                    res.should.have.status(422);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('errors');
+                    res.body.errors.should.have.length(6);
+                    done();
+                });
+        });
+
+        context('#safety out of range (1-5) ', () => {
+
+            it('should return error when safety value is less than 1', (done) => {
                 let assessment = {
+                    assessmentId: 195564,
+                    vendorName: 'Apple Inc',
+                    safety: 0,
+                    safetyComment: 'Very safe equipment',
+                    quality: 'Good',
+                    qualityComment: 'Exceeds expectation',
+                    Notes: 'None'
+                };
+
+                chai.request(server)
+                    .post('/api/assessment')
+                    .send(assessment)
+                    .end((err, res) => {
+                        res.should.have.status(422);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('errors');
+                        res.body.errors[0].should.have.property('param').eql('safety');
+                        done();
+                    });
+
+            });
+
+            it('should return error when safety value is less than 1', (done) => {
+                let assessment = {
+                    assessmentId: 195564,
+                    vendorName: 'Apple Inc',
+                    safety: 6,
+                    safetyComment: 'Very safe equipment',
+                    quality: 'Good',
+                    qualityComment: 'Exceeds expectation',
+                    Notes: 'None'
+                };
+
+                chai.request(server)
+                    .post('/api/assessment')
+                    .send(assessment)
+                    .end((err, res) => {
+                        res.should.have.status(422);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('errors');
+                        res.body.errors[0].should.have.property('param').eql('safety');
+                        done();
+                    });
+            });
+        });
+
+        context('# quality values', () => {
+            it('should return error when quality does not belong to Good/Bad/Average/Excellent', (done)=>{
+                let assessment = {
+                    assessmentId: 195564,
+                    vendorName: 'Apple Inc',
+                    safety: 2,
+                    safetyComment: 'Safe',
+                    quality: 'Better',
+                    qualityComment: '',
                     Notes: 'None'
                 };
 
                 chai.request(server)
                 .post('/api/assessment')
                 .send(assessment)
-                .end((err, res) =>{
+                .end((err, res) => {
                     res.should.have.status(422);
                     res.body.should.be.a('object');
                     res.body.should.have.property('errors');
-                    res.body.errors.should.have.length(6);
-                   done();  
+                    res.body.errors[0].should.have.property('param').eql('quality');
+                    done();
                 });
-            }); 
+            });
+           
+        });
     });
-     
-
 
 });
