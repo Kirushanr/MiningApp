b655c17dc0e84d3cc66a6ed3a85bb3d913c6b74b
kirushan <kirushan22@gmail.com>
2019-04-14 18:10:54 +1000

    remove deprecation warning

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const env = require('dotenv').config();
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(process.env.DB_HOST,{ useNewUrlParser: true });
+mongoose.connect(process.env.DB_HOST,{ useNewUrlParser: true,useCreateIndex: true });
 
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
@@ -22,3 +22,5 @@ app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+module.exports =app;
