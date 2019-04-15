d819658c24361623e25afa81381c90512827b6fd
kirushan <kirushan22@gmail.com>
2019-04-15 15:41:10 +1000

    Add middleware, and routes for Google authentication

---
+++ routes/google_route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const {generateToken, sendToken, verifyToken} = require('../auth/token');
+const passport = require('passport');
+
+
+//Google Authentication route
+router.get('/auth/google',passport.authenticate('google', { scope: ['profile'] }));
+router.get('/auth/google/callback',passport.authenticate('google', { failureRedirect: '/auth/google' }),
+ function(req,res,next) {
+    
+  req.auth = {
+      id: req.id
+  };
+  next();
+},generateToken, sendToken);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const assessment= require('../controllers/assessmentController'); 
-const validator = require('../validator');
+const {validate} = require('../validator');
+const {verifyToken} = require('../auth/token');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/assessment/:id',validator.validate('AssessmentId'),assessment.getAssesment);
-router.post('/assessment',validator.validate('Assessment'),assessment.createAssesment);
+router.get('/assessment/:id',[verifyToken,validate('AssessmentId'),assessment.getAssesment]);
+router.post('/assessment',[verifyToken,validate('Assessment'),assessment.createAssesment]);
 
 
 
