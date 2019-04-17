5bc13a63698a46f569fc3e4a8afdceb39cbcccc1
kirushan <kirushan22@gmail.com>
2019-04-18 00:35:42 +1000

    Rename paramenter
    Rename router parameter id to assessmentId

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,13 @@ const {verifyToken} = require('../auth/token');
 
 router.get('/assessments',[verifyToken,assessment.getAssessments]);
 
-router.get('/assessments/:id',[verifyToken,validate('AssessmentId'),assessment.getAssessment]);
+router.get('/assessments/:assessmentId',[verifyToken,validate('AssessmentId'),assessment.getAssessment]);
 
 router.post('/assessments',[verifyToken,validate('Assessment'),assessment.createAssessment]);
 
-router.delete('/assessments/:id',[verifyToken,validate('AssessmentId'),assessment.deleteAssessment]);
+router.delete('/assessments/:assessmentId',[verifyToken,validate('AssessmentId'),assessment.deleteAssessment]);
 
-router.put('/assessments/:id',[verifyToken,validate('Assessment'),assessment.updateAssessment]);
+router.put('/assessments/:assessmentId',[verifyToken,validate('Assessment'),assessment.updateAssessment]);
+
 
 module.exports = router;
